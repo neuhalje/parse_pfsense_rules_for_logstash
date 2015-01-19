48c1ae19d63dc27d91fcc325ff0e95339d2e0213
Jens Neuhalfen <jens@neuhalfen.name>
2015-01-19 19:12:56 +0100

    Add very basic parsing.

--- pfsense_parser/parser.py
+++ pfsense_parser/parser.py
@@ -1,4 +1,23 @@
+import re
+
 class Parser(object):
     def parse(self, input):
-        return []
+        self.result = []
 
+        if isinstance(input, basestring):
+            self._parse_line(input)
+        else:
+            for line in input:
+                self._parse_line(line)
+
+        return self.result
+
+    def _parse_line(self, line):
+        #@0 scrub on vr2 all fragment reassemble
+        match = re.match('^@(\d+)\s+(\w+)\s+(.*)$', line)
+        if match:
+            id = int(match.group(1))
+            type = match.group(2)
+            msg = match.group(3)
+
+            self.result.append({"id": id, "type": type, "msg": msg })
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -15,6 +15,26 @@ class ParserTest(unittest.TestCase):
         sut = self.create_sut()
         self.assertEquals([], sut.parse("\n\n\n"))
 
+    def test_empty_lines_as_list__parses_to__empty_list(self):
+        sut = self.create_sut()
+        self.assertEquals([], sut.parse(["\n","\n"]))
+
+    def test_garbage_string__parses_to__empty_list(self):
+        sut = self.create_sut()
+        self.assertEquals([], sut.parse(" [ Evaluations: 7788788   Packets: 6477312   Bytes: 1941991782  States: 0     ]"))
+
+    def test_scrub_line__parses_to__list_with_scrub_entry(self):
+        sut = self.create_sut()
+        self.assertEquals([{"id":0, "type": "scrub", "msg":"on vr2 all fragment reassemble"}], sut.parse("@0 scrub on vr2 all fragment reassemble"))
+
+    def test_scrub_line_as_list__parses_to__list_with_scrub_entry(self):
+        sut = self.create_sut()
+        self.assertEquals([{"id":0, "type": "scrub", "msg":"on vr2 all fragment reassemble"}], sut.parse(["@0 scrub on vr2 all fragment reassemble"]))
+
+    def test_two_calls__parses_to__one_result(self):
+        sut = self.create_sut()
+        sut.parse(["@0 scrub on vr2 all fragment reassemble"])
+        self.assertEquals([{"id":0, "type": "scrub", "msg":"on vr2 all fragment reassemble"}], sut.parse(["@0 scrub on vr2 all fragment reassemble"]))
 
 if __name__ == '__main__':
     unittest.main()
