13245b6482097daca6091f83cfbf4a70137e881e
Jens Neuhalfen <jens@neuhalfen.name>
2015-01-21 21:48:41 +0100

    Update package

--- setup.py
+++ setup.py
@@ -7,25 +7,18 @@ except ImportError:
 config = {
     'description': 'Parse pfSense firewall rules. Supported: pfSense 2.1',
     'author': 'Jens Neuhalfen',
-    'url': 'TODO',
-    'download_url': 'TODO',
-    'author_email': 'TODO',
+    'url': 'https://github.com:neuhalje/parse_pfsense_rules_for_logstash',
+    'download_url': 'https://github.com/neuhalje/parse_pfsense_rules_for_logstash/zipball/master',
     'version': '0.1',
     'install_requires': ['argparse', 'nose'],
     'packages': ['pfsense_parser', 'cli'],
     'name': 'pfsenseparser',
     'license': 'BSD',
-    'classifiers': [
-            "Development Status :: 3 - Alpha",
+    'classifiers': [ "Development Status :: 3 - Alpha",
             "Topic :: Utilities",
-            "License :: OSI Approved :: BSD License",
-                   ],
-    'entry_points':{
-             'console_scripts': [
-                   'pfSense2Logstash=cli.cli:main',
-                                ],
-             'gui_scripts': [ ]
-                   }
+            "License :: OSI Approved :: BSD License", ],
+    'entry_points':{ 'console_scripts': [ 'pfSense2Logstash=cli.cli:main', ],
+             'gui_scripts': [ ] }
     }
 
 setup(**config)
