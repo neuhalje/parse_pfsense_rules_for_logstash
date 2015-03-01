c759f7d695852d07ff1eec8e6d66ebb8a439ac23
Jens Neuhalfen <jens@neuhalfen.name>
2015-03-01 18:28:43 +0100

    run_tests: Install python environment

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,37 @@
-#!/bin/sh
- . ~/.python/parse-pfsense-python2.7/bin/activate
+#!/usr/bin/env bash
+
+eval venv_root="~/.python"
+venv="parse-pfsense-python2.7"
+
+
+function install_venv()
+{
+        prompt="Install python virtualenv in $venv_root/$venv [y/n]"
+        answer=""
+
+        while [[ ! "$answer" =~ ^[yn]$  ]]
+        do
+           read -p "$prompt" -n 1 answer 
+           echo 
+        done
+
+        if [ ! x$answer == "xy" ]; then exit 1;fi
+
+        mkdir -p "$venv_root"
+        virtualenv --python=python2.7 "$venv_root/$venv"
+}
+
+if [ ! -f $venv_root/$venv/bin/activate ]
+then
+        install_venv
+fi
+
+. "$venv_root/$venv/bin/activate"
+
+which nosetests >/dev/null
+if [ ! -z $? ]
+then
+  pip install nose
+fi
+
 nosetests
